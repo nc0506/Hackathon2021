60f3e3ae7ce45843204207e1bf8304d5e5c57b6e
nc0506 <38858537+nc0506@users.noreply.github.com>
2021-04-10 17:04:32 +0530

    add to Hackathon2021 under nitin

---
+++ Nitin_Hiwarkar/Hackathon2021/Hackathon2021/Repository/Cricket.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hackathon2021.DBContext;
+using Hackathon2021.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hackathon2021.Repository
+{
+    public class Cricket : ICricket
+    {
+        private readonly CricketContext _dbContext;
+
+        public Cricket(CricketContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void DeleteMatch(int MatchID)
+        {
+            var data = _dbContext.Data.Find(MatchID);
+            _dbContext.Data.Remove(data);
+            Save();
+        }
+
+        public Data GetMatchByID(int MatchID)
+        {
+            return _dbContext.Data.Find(MatchID);
+        }
+
+        public IEnumerable<Data> GetMatchData()
+        {
+            return _dbContext.Data.ToList();
+        }
+
+        public void InsertMatch(Data CricketData)
+        {
+            _dbContext.Add(CricketData);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void  UpdateMatch(Data CricketData)
+        {
+            _dbContext.Entry(CricketData).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
---
+++ Nitin_Hiwarkar/Hackathon2021/Hackathon2021/Repository/ICricket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hackathon2021.Model;
+
+namespace Hackathon2021.Repository
+{
+    public interface ICricket
+    {
+        IEnumerable<Data> GetMatchData();
+        Data GetMatchByID(int MatchID);
+        void InsertMatch(Data CricketData);
+        void DeleteMatch(int MatchID);
+        void UpdateMatch(Data CricketData);
+        void Save();
+    }
+}
+
